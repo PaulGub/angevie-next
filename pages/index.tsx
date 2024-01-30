fdb046aea7c3787afc96bbab61a88c97ad7fbc06
Paul Gubbiotti <90463998+PaulGub@users.noreply.github.com>
2024-01-30 18:27:44 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,8 +13,8 @@ const Home = () => {
                     <a href="mailto:AngeEtVie@laposte.net" className="text-blue-500">AngeEtVie@laposte.net</a> – <a href="tel:0767055790" className="text-blue-500">07 67 05 57 90</a>
                 </p>
                 <p>
-                    Actualité : week-end à Brocéliande du 29 septembre au 1er octobre 2023.{' '}
-                    <a target="blank" href="https://www.facebook.com/events/304204702171842/?acontext=%7B%22event_action_history%22%3A[%7B%22extra_data%22%3A%22%22%2C%22mechanism%22%3A%22left_rail%22%2C%22surface%22%3A%22bookmark%22%7D%2C%7B%22extra_data%22%3A%22%22%2C%22mechanism%22%3A%22calendar_tab_event%22%2C%22surface%22%3A%22bookmark_calendar%22%7D]%2C%22ref_notif_type%22%3Anull%7D" className="text-blue-500">
+                    Actualité : week-end à Brocéliande du 05 septembre au 07 avril 2024.{' '}
+                    <a target="blank" href="https://www.facebook.com/profile.php?id=100054545855781" className="text-blue-500">
                         Cliquez sur le lien pour plus d’informations.
                     </a>
                 </p>
